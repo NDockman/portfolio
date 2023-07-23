39d0b5d5d69ff42e2d72b71982bfc709e203c2a9
DockDock <ndockman@comcast.net>
2023-07-23 12:07:00 -0500

    Added bio and modified project flexbox.

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -7,7 +7,7 @@ function Bio() {
 
   return (
     <div class="about">
-      <p>Hi, nice to meet you! My name is Noah and I am a web developer. Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, eligendi perspiciatis nihil, sit in modi dolores excepturi ex reprehenderit officia at sunt nostrum deserunt aperiam numquam recusandae eius accusantium facere!</p>
+      <p>Hi, nice to meet you! My name is Noah and I am a web developer. I have always been fascinated by technology and computers. In high school and college I took introductory courses for computer science and knew that I had found my calling. I received certification for full stack web development through the University of Minnesota. Besides coding, my interests include movies, video games, basketball, hanging out with dogs, and reading history books.</p>
     </div>
   );
 }
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
-import gradeimg from "../images/gradebook-homepage-screenshot2023-06-10.png";
-import fishimg from "../images/mfc-profile.png";
+import gradeimg from "../images/gradebook-screenshot.png";
+import fishimg from "../images/mfc-screenshot.png";
 import shoppimg from "../images/shopping-demo-screenshot.png";
 
 
@@ -12,22 +12,24 @@ const Project = () => {
   return (
     <section class="project-container">
       <div class="big-project">
-        <h1>Gradebook</h1>
+        {/* <h1>Gradebook</h1> */}
         <a href="https://umb-gradebook.herokuapp.com/" target="_blank">
           <img class="big-project-sc"src={gradeimg} alt="Screenshot for gradebook website"/>
         </a>
       </div>
-      <div class="reg-project">
-        <h2>Minnesota Fresh Catch</h2>
-        <a href="https://minnesota-fresh-catch.herokuapp.com/" target="_blank">
-          <img class="project-sc" src={fishimg} alt="Screenshot for Minnesota Fresh Catch website"/>
-        </a>
-      </div>
-      <div class="reg-project">
-        <h2>Shoppi</h2>
-        <a href="https://moe1401.github.io/shopping-demo/" target="_blank">
-          <img class="project-sc" src={shoppimg} alt="Screenshot for Shopping Demo website"/>
-        </a>
+      <div class="project-container-two">
+        <div class="reg-project">
+          {/* <h2>Minnesota Fresh Catch</h2> */}
+          <a href="https://minnesota-fresh-catch.herokuapp.com/" target="_blank">
+            <img class="project-sc" src={fishimg} alt="Screenshot for Minnesota Fresh Catch website"/>
+          </a>
+        </div>
+        <div class="reg-project">
+          {/* <h2>Shoppi</h2> */}
+          <a href="https://moe1401.github.io/shopping-demo/" target="_blank">
+            <img class="project-sc" src={shoppimg} alt="Screenshot for Shopping Demo website"/>
+          </a>
+        </div>
       </div>
 
     </section>
