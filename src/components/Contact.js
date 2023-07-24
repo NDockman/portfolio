a0f54f91e441aee60028ce3a4d0e875e837debdb
DockDock <ndockman@comcast.net>
2023-07-24 15:23:17 -0500

    Added logos for contacts. Modified CSS.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
-
+import gmailimg from "../images/gmail-logo.png";
+import githubimg from "../images/github-logo2.png";
+import linkedinimg from "../images/linkedin-logo.png";
 
 function Contact() {
 
@@ -9,15 +11,21 @@ function Contact() {
     <div class="outro">
       
       <div>
-        <ul>
+        <ul class="contact-container">
           <li>
-            <a href="mailto:xnsd2015@gmail.com">xnsd2015@gmail.com</a>
+            <a href="mailto:xnsd2015@gmail.com">xnsd2015@gmail.com
+              <img class="logo"src={gmailimg} alt="gmail logo"/>
+            </a>
           </li>
           <li>
-            <a href="http://github.com/NDockman" target="_blank">GitHub</a>
+            <a href="http://github.com/NDockman" target="_blank">GitHub
+              <img class="logo"src={githubimg} alt="github logo"/>
+            </a>
           </li>
           <li>
-            <a href="">LinkedIn</a>
+            <a href="https://www.linkedin.com/in/noah-dockman-61a635277/">LinkedIn
+              <img class="logo"src={linkedinimg} alt="linkedin logo"/>
+            </a>
           </li>
         </ul>
       </div>
